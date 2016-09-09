379ab0fd8ec01a65131475af6ca58f46cbe02d94
ian <redbluse@gmail.com>
2016-09-09 05:54:59 +0000

    httpsever

--- JS/server/server.js
+++ JS/server/server.js
@@ -1,3 +1,17 @@
-console.log('hello world');
-console.log(1+1);
+const http = require('http');
+
+const hostname = '52.238.242.209';
+
+const port = 1337;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World\n');
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
+
 
