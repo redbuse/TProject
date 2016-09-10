d17d21580c7f0b1c2ac0e848aa38f916492b0e60
Ian <redbluse@gmail.com>
2016-09-11 01:26:07 +0900

    add

--- app.js
+++ app.js
@@ -3,8 +3,15 @@ var path = require('path');
 
 var app = express();
 
+app.set("view engine", 'ejs');
+
+
 app.use(express.static(path.join(__dirname , '/public')));
 
+app.get('/',function (req,res){
+  res.render('my_first_ejs');
+
+});
 app.listen(8000,function(){
 
   console.log('Server on!');
