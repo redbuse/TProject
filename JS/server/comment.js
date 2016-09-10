298082584d92605e69a8643c51091bc19d8630bd
Ian <redbluse@gmail.com>
2016-09-11 02:50:21 +0900

    change

---
+++ JS/server/comment.js
@@ -0,0 +1,32 @@
+var express = require('express');
+
+var mysql = require('mysql');
+
+var route = express.Router();
+
+
+var connection = mysql.createConnection({
+  host:"localhost",
+  port:8000,
+  user:"root",
+  password:"apmsetup",
+  database:"nodejs"
+
+});
+
+
+var sqlQuery = "INSERT INTO member SET ?";
+var post = {id:"redbluse",pw:"nbnl2016", name:"ttalk"};
+
+function callback(err,result){
+  if(err){
+    throw err;
+  }
+
+  console.log("Insert Complete!");
+  console.log(query.sql);
+}
+
+connection.connect();
+var query = connection.query(sqlQuery,post,callback);
+connection.end();
